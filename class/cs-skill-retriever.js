1c5d051818ab7fd9691558fa6d502f3bdd641f0b
rnielikki <lumi.kwon@student.lamk.fi>
2022-07-01 23:39:58 +0300

    checkbox auto update works

--- class/cs-skill-retriever.js
+++ class/cs-skill-retriever.js
@@ -95,22 +95,30 @@
     window.addEventListener("load", function () {
         _target = document.querySelector(".Type");
         
-        const groupMap = new Map();
+        const groupMap = {};
         const inputGroup = document.querySelector("#skill-input-group");
         const inputGroupRadios = [...inputGroup.querySelectorAll("[data-type]")];
 
-        const detailMap = new Map();
         const inputDetail = document.querySelector("#skill-input-detail");
         const allDetails = [...inputDetail.querySelectorAll("[data-skill-detail]")]; 
         
         for(const detail of allDetails)
         {
-            detailMap.set(detail.dataset.skillDetail, detail);
+            groupMap[detail.dataset.skillDetail] = {};
+            groupMap[detail.dataset.skillDetail]["detail"]=detail;
+            groupMap[detail.dataset.skillDetail]["detail-content"]={};
+            const detailContentMap = groupMap[detail.dataset.skillDetail]["detail-content"];
+            for(const detailContent of [...detail.querySelectorAll("[data-skill-index]")])
+            {
+                if(!detailContentMap[detailContent.dataset.skillIndex]) detailContentMap[detailContent.dataset.skillIndex] = {};
+                if(!detailContent.dataset.skillValue) detailContentMap[detailContent.dataset.skillIndex] = detailContent;
+                else detailContentMap[detailContent.dataset.skillIndex][detailContent.dataset.skillValue.toLowerCase()] = detailContent;
+            }
             detail.style.display = "none";
         }
         for(const toggle of inputGroupRadios)
         {
-            groupMap.set(toggle.dataset.type, toggle);
+            groupMap[toggle.dataset.type]["toggle"] = toggle;
             toggle.addEventListener("change", function(e){
                 updateLegend(e.target.dataset.type);
             })
@@ -118,32 +126,62 @@
 
         window.addEventListener("skillLoaded", function(e) {
             const type = e.detail.Type;
-            if(groupMap.has(type)) {
-                groupMap.get(type).checked = true;
-                updateLegend(type);
+            if(groupMap[type]) {
+                groupMap[type].toggle.checked = true;
+                updateLegend(type, e.detail);
             }
             else{
                 _target.textContent = "Other";
                 for(const toggle of inputGroupRadios) {
                     toggle.checked = false;
                 }
-                updateLegend("");
+                updateLegend("", null);
             }
         });
+        console.log(groupMap);
         addUpdaters("Attack");
         addUpdaters("Command");
         addUpdaters("Status Effect");
         
-        function updateLegend(dataType) {
+        function updateLegend(dataType, skill) {
             for(const detail of allDetails) {
                 detail.style.display=(dataType === detail.dataset.skillDetail)?"block":"none";
             }
             if(!dataType) return;
+            if(skill){
+                const detailContents = groupMap[dataType]["detail-content"];
+                for(const detailIndex of Object.keys(detailContents)) {
+                    const skillDetailData = skill[detailIndex];
+                    //array
+                    if(Array.isArray(skillDetailData)) {
+                        for(const detailKey of Object.keys(detailContents[detailIndex])) {
+                            const skillDetail = detailContents[detailIndex][detailKey].dataset.skillValue;
+                            const skillDetailLower = skillDetail.toLowerCase();
+                            detailContents[detailIndex][skillDetailLower].checked = skillDetailData.findIndex(k => k.toLowerCase() === detailKey) > -1;
+                        }
+                    }
+                    //true/false checkbox
+                    else if(typeof(skillDetailData)==="boolean") {
+                        if(detailContents[detailIndex]) {
+                            detailContents[detailIndex].checked = skillDetailData;
+                        }
+                    }
+                    //not an array
+                    else {
+                        const elem = detailContents[detailIndex][skillDetailData.toLowerCase()];
+                        //console.log(`${skillDetailData} and ${detailIndex}`);
+                        if(skillDetailData && detailContents[detailIndex]) {
+                            elem.checked =
+                                skillDetailData.toLowerCase() === elem.dataset.skillValue.toLowerCase();
+                        }
+                    }
+                }
+            }
             const calculator = calculators[dataType];
-            if(calculator)  calculator();
+            if(calculator) calculator();
         }
         function addUpdaters(type) {
-            for(const elem of [...detailMap.get(type).querySelectorAll("input")]){
+            for(const elem of [...groupMap[type].detail.querySelectorAll("input")]){
                 elem.addEventListener("change", calculators[type]);
             }
         }
